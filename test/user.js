bb7a7dc3f1d667be0f7e556e1f87af26002d3e39
Saidi <rasheedsaidi@gmail.com>
2018-04-17 07:36:06 +0100

    Write tests for /login endpoint

--- test/user.js
+++ test/user.js
@@ -30,4 +30,21 @@ describe('User', () => {
         });
     });
   });
+
+  describe('/POST login user', () => {
+    it('should authenticate a user with a valid credential', (done) => {
+      let login = {username: 'admin', password: 'secret'};
+
+      chai.request(app)
+        .post('/api/v1/users/login')
+        .send(login)
+        .end( (err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('error').eql('false');
+          res.body.should.have.property('reply').eql('Login successful');
+          done();
+        })
+    });
+  })
 });
